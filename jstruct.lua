f83de86e18aa84e5c4629e16e917ccc969f9b98b
Grant Haywood <grant@iowntheinter.net>
2016-11-26 17:47:32 -0700

    need this

--- jstruct.lua
+++ jstruct.lua
@@ -1,4 +1,24 @@
 local JSON = (loadfile "ext/JSON.lua")() -- one-time load of the routines
+--https://github.com/torch/xlua/blob/98c11caac263e21f793ab2e6a8ed536b3c7cb135/init.lua#L689-L706
+function string.split(str, pat)
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+         table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
+
 function l_decodeJson (decodeVar) 
   local jstring = bash.getVariable(decodeVar)
   local jsont = JSON:decode(jstring)
